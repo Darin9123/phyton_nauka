189c8019f92b9509b8febd3f46d2f28a9ebeb1f8
Darin9123 <amichal@gmail.com>
2022-03-07 19:39:37 +0100

    Poprawiona wersja:

---
+++ figuryenumeracja.py
@@ -0,0 +1,62 @@
+import math
+wybor = -1
+from enum  import IntEnum
+Menu_Figury = IntEnum("Menu_Figury", 'Kwadrat Prostakat Trojkat Trapez Koło') 
+
+def kwadrat(a):
+    pole = a * a
+    print ( "Pole kwadratu wynosi " , pole)
+
+def pole_prostakata(a  , b):
+    pole = a * b 
+    print("Pole prostakata wynosi" , pole)
+
+def pole_troj(a , h) :
+    pole =  0.5 * a * h
+    print ("Pole trojkata wynosi " , pole)
+
+def pole_trapez(a , b, h) :
+    pole = (a+b)/2 * h
+    print("Pole trapezu wynosi" , pole)
+
+def pole_kola(r):
+    pole = math.pi * r**2
+    print("Pole koła wynosi " , pole)
+
+while(wybor != 6):
+    if wybor == Menu_Figury.Kwadrat:
+        bok = int(input("Podaj bok :"))
+        kwadrat(bok)
+    elif wybor ==Menu_Figury.Prostakat:
+        bok1 = int(input("Podaj Pierwszy bok "))
+        bok2 = int(input("Podaj drugi bok "))
+        pole_prostakata(bok1, bok2)
+    elif wybor ==Menu_Figury.Trojkat :
+        bok = int(input("Podaj Bok trójkata "))
+        wys = int(input("Podaj wysokosc trojkata "))
+        pole_troj(bok , wys)
+    elif wybor == Menu_Figury.Trapez:
+         bok1 = int(input("Podaj Pierwszy bok "))
+         bok2 = int(input("Podaj drugi bok "))
+         wys = int(input("Podaj wysokosc "))
+         pole_trapez(bok1 , bok2, wys)
+    elif wybor == Menu_Figury.Koło :
+         promien = int(input("Podaj promien  ")) 
+         pole_kola(promien)  
+
+
+
+
+
+    print()
+    print("1. Policz pole kwadratu")
+    print("2.Policz pole prostokata")
+    print("3. Policz pole trójkata ")
+    print("4. Policz pole trapezu")
+    print("5. Policz pole kola")
+    print("6. Wyjdz z programu ")
+
+    wybor = int(input("Wybierz co chesz zrobic  :"))
+    print()
+
+     
